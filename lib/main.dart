e7ccd3de0ae62e8e059d07d7fee7083eb91c41de
Traveling Lumine <107802667+traveling-lumine@users.noreply.github.com>
2023-08-24 02:28:49 +0900

    Split some files

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,17 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:genshin_mod_manager/app_state.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:window_manager/window_manager.dart';
 
-import 'fsops.dart';
-import 'page/folder.dart';
-import 'page/setting.dart';
+import 'home.dart';
 
 void main() async {
-  fallBackApp();
   await initialize();
-  AppState appState = await getAppState();
-  runApp(MyApp(appState));
-}
-
-void fallBackApp() {
-  runApp(
-    const Center(
-      child: Directionality(
-        textDirection: TextDirection.ltr,
-        child: Text(
-          'Loading settings.'
-          ' If this screen persists,'
-          ' please report the problem to developer.',
-        ),
-      ),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 Future<void> initialize() async {
@@ -43,203 +24,61 @@ Future<void> initialize() async {
   });
 }
 
-Future<AppState> getAppState() async {
-  final instance = await SharedPreferences.getInstance();
-  final String targetDir = instance.getString('targetDir') ?? '.';
-  final String launcherDir = instance.getString('launcherDir') ?? '.';
-  final appState = AppState(targetDir, launcherDir);
-  return appState;
-}
-
 class MyApp extends StatelessWidget {
-  final AppState appState;
+  static Logger logger = Logger();
 
-  const MyApp(this.appState, {super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return FluentApp(
       title: 'Genshin Mod Manager',
-      home: ChangeNotifierProvider.value(
-        value: appState,
-        builder: (context, child) {
-          return const MyHomePage();
+      home: FutureBuilder(
+        future: getAppState().timeout(
+          const Duration(seconds: 1),
+          onTimeout: () {
+            return AppState('.', '.');
+          },
+        ),
+        builder: (context, snapshot) {
+          logger.i('App FutureBuilder snapshot status: $snapshot');
+          if (!snapshot.hasData) {
+            return buildLoadingScreen();
+          }
+          return buildMain(snapshot);
         },
       ),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> with WindowListener {
-  String? targetDir;
-  late Directory watchDir;
-
-  StreamSubscription<FileSystemEvent>? watcher;
-  late List<NavigationPaneItem> subFolders;
-  int? selected;
-
-  void updateFolder(String tDir) {
-    targetDir = tDir;
-    watchDir = Directory(targetDir!);
-    watcher?.cancel();
-    subFolders = [];
-    selected = null;
-
-    try {
-      getAllChildrenFolder(targetDir!).forEach((element) {
-        subFolders.add(FolderPaneItem(folder: element));
-      });
-    } on PathNotFoundException {
-      print('Path not found: $targetDir');
-    }
-    watcher = watchDir.watch().listen((event) {
-      setState(() {
-        final NavigationPaneItem? prevSelItem;
-        if (selected != null) {
-          prevSelItem = subFolders[selected!];
-        } else {
-          prevSelItem = null;
-        }
-        subFolders = [];
-        getAllChildrenFolder(targetDir!).forEach((element) {
-          subFolders.add(FolderPaneItem(folder: element));
-        });
-        if (prevSelItem != null) {
-          // find the index of prevSelItem using key
-          final index = subFolders.indexWhere((element) {
-            return element.key == prevSelItem!.key;
-          });
-          if (index != -1) {
-            selected = index;
-          }
-        }
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    windowManager.addListener(this);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    watcher?.cancel();
-    windowManager.removeListener(this);
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var targetDir2 =
-        '${context.select<AppState, String>((value) => value.targetDir)}\\Mods';
-    if (targetDir == null) {
-      targetDir = targetDir2;
-      updateFolder(targetDir!);
-    } else if (targetDir != targetDir2) {
-      updateFolder(targetDir2);
-    }
-    return NavigationView(
-      transitionBuilder: (child, animation) {
-        return FadeTransition(
-          opacity: animation,
-          child: child,
-        );
+  Widget buildMain(AsyncSnapshot<AppState> snapshot) {
+    return ChangeNotifierProvider.value(
+      value: snapshot.data,
+      builder: (context, child) {
+        return const MyHomePage();
       },
-      appBar: const NavigationAppBar(
-        title: DragToMoveArea(
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Text('Genshin Mod Manager'),
-          ),
+    );
+  }
+
+  Widget buildLoadingScreen() {
+    return const ScaffoldPage(
+      content: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ProgressRing(),
+            Text('Loading...'),
+          ],
         ),
-        automaticallyImplyLeading: false,
-        actions: WindowButtons(),
-      ),
-      pane: NavigationPane(
-        selected: selected,
-        onChanged: (i) => setState(() => selected = i),
-        displayMode: PaneDisplayMode.auto,
-        size: const NavigationPaneSize(openWidth: 300),
-        autoSuggestBox: AutoSuggestBox(
-          items: subFolders
-              .map((e) => AutoSuggestBoxItem(
-                    value: e.key,
-                    label: (e as FolderPaneItem).folder.split('\\').last,
-                  ))
-              .toList(),
-          trailingIcon: const Icon(FluentIcons.search),
-          onSelected: (item) {
-            setState(() {
-              selected =
-                  subFolders.indexWhere((element) => element.key == item.value);
-            });
-          },
-        ),
-        autoSuggestBoxReplacement: const Icon(FluentIcons.search),
-        items: subFolders,
-        footerItems: [
-          PaneItemSeparator(),
-          PaneItem(
-            icon: const Icon(FluentIcons.user_window),
-            title: const Text('3d migoto'),
-            body: Center(child: Image.asset('images/app_icon.ico')),
-            onTap: () {
-              final path =
-                  '${context.read<AppState>().targetDir}\\3DMigoto Loader.exe';
-              runProgram(path);
-              print('run!');
-            },
-          ),
-          PaneItem(
-            icon: const Icon(FluentIcons.user_window),
-            title: const Text('Launcher'),
-            body: Center(child: Image.asset('images/app_icon.ico')),
-            onTap: () {
-              runProgram(context.read<AppState>().launcherDir);
-            },
-          ),
-          PaneItem(
-            icon: const Icon(FluentIcons.settings),
-            title: const Text('Settings'),
-            body: const SettingPage(),
-          ),
-        ],
       ),
     );
   }
-}
 
-class FolderPaneItem extends PaneItem {
-  String folder;
-
-  FolderPaneItem({
-    required this.folder,
-  }) : super(
-          title: Text(folder.split('\\').last),
-          icon: Image.asset('images/app_icon.ico'),
-          body: FolderPage(folder: folder),
-          key: ValueKey(folder),
-        );
-}
-
-class WindowButtons extends StatelessWidget {
-  const WindowButtons({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(
-      width: 138,
-      height: 50,
-      child: WindowCaption(),
-    );
+  Future<AppState> getAppState() async {
+    final instance = await SharedPreferences.getInstance();
+    final String targetDir = instance.getString('targetDir') ?? '.';
+    final String launcherDir = instance.getString('launcherDir') ?? '.';
+    final appState = AppState(targetDir, launcherDir);
+    return appState;
   }
 }
