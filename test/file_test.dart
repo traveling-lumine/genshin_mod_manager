9b23141bd57059b189c4e5b212d2c0972dd50305
Traveling Lumine <107802667+traveling-lumine@users.noreply.github.com>
2024-03-16 11:28:19 +0900

    Add file benchmark tests

--- test/file_test.dart
+++ test/file_test.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Benchmark speed', () async {
+  test('Benchmark listing', () async {
     var stopwatch = Stopwatch()..start();
-    const other = 10000;
+    const other = 100;
     for (var i = 0; i < other; i++) {
       Directory('.').listSync();
     }
@@ -15,6 +15,47 @@ void main() {
       await Directory('.').list().toList();
     }
     final asyncDuration = stopwatch.elapsed;
+    print('Sync: ${syncDuration.inMicroseconds / other}'
+        ', async: ${asyncDuration.inMicroseconds / other}');
+    expect(syncDuration, lessThan(asyncDuration));
+  });
+  test('Benchmark deleting', () async {
+    var stopwatch = Stopwatch()..start();
+    const other = 100;
+    final directory = Directory('t_dir');
+    for (var i = 0; i < other; i++) {
+      stopwatch.stop();
+      directory.createSync();
+      stopwatch.start();
+      directory.deleteSync(recursive: true);
+    }
+    final syncDuration = stopwatch.elapsed;
+    stopwatch = Stopwatch()..start();
+    for (var i = 0; i < other; i++) {
+      stopwatch.stop();
+      await directory.create();
+      stopwatch.start();
+      await directory.delete(recursive: true);
+    }
+    final asyncDuration = stopwatch.elapsed;
+    print('Sync: ${syncDuration.inMicroseconds / other}'
+        ', async: ${asyncDuration.inMicroseconds / other}');
+    expect(syncDuration, lessThan(asyncDuration));
+  });
+  test("Benchmark exists", () async {
+    const other = 100;
+    var stopwatch = Stopwatch()..start();
+    for (var i = 0; i < other; i++) {
+      Directory('.').existsSync();
+    }
+    final syncDuration = stopwatch.elapsed;
+    stopwatch = Stopwatch()..start();
+    for (var i = 0; i < other; i++) {
+      await Directory('.').exists();
+    }
+    final asyncDuration = stopwatch.elapsed;
+    print('Sync: ${syncDuration.inMicroseconds / other}'
+        ', async: ${asyncDuration.inMicroseconds / other}');
     expect(syncDuration, lessThan(asyncDuration));
   });
 }
