98d33419a5732fedf8d9c63925e23c7e9937e7a2
Traveling Lumine <107802667+traveling-lumine@users.noreply.github.com>
2024-04-06 00:52:19 +0900

    Improve UX

--- lib/ui/widget/category_drop_target.dart
+++ lib/ui/widget/category_drop_target.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:desktop_drop/desktop_drop.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:genshin_mod_manager/data/helper/copy_directory.dart';
 import 'package:genshin_mod_manager/data/helper/path_op_string.dart';
 import 'package:genshin_mod_manager/domain/entity/mod_category.dart';
@@ -11,21 +12,88 @@ import 'package:genshin_mod_manager/flow/app_state.dart';
 import 'package:genshin_mod_manager/ui/util/display_infobar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CategoryDropTarget extends ConsumerWidget {
+/// A widget that acts as a drop target for files and directories.
+class CategoryDropTarget extends HookConsumerWidget {
+  /// Creates a [CategoryDropTarget].
   const CategoryDropTarget({
     required this.child,
     required this.category,
     super.key,
   });
 
+  /// The child widget.
   final Widget child;
+
+  /// The category to drop the files into.
   final ModCategory category;
 
   @override
-  Widget build(final BuildContext context, final WidgetRef ref) => DropTarget(
-        onDragDone: (final details) => _onDragDone(details, context, ref),
-        child: child,
-      );
+  Widget build(final BuildContext context, final WidgetRef ref) {
+    final state = useState(false);
+    return DropTarget(
+      onDragEntered: (final details) {
+        state.value = true;
+      },
+      onDragExited: (final details) {
+        state.value = false;
+      },
+      onDragDone: (final details) => _onDragDone(details, context, ref),
+      child: Stack(
+        children: [
+          child,
+          Positioned.fill(
+            child: _buildDropHint(ref, state),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AnimatedOpacity _buildDropHint(
+    final WidgetRef ref,
+    final ValueNotifier<bool> state,
+  ) {
+    final context = useContext();
+    final moveMethod = ref.watch(
+      appStateNotifierProvider.select((final value) => value.moveOnDrag),
+    )
+        ? 'move'
+        : 'copy';
+    final text = RichText(
+      text: TextSpan(
+        text: 'Drop to $moveMethod to',
+        style: FluentTheme.of(context).typography.body,
+        children: [
+          TextSpan(
+            text: ' ${category.name}',
+            style: FluentTheme.of(context).typography.bodyStrong,
+          ),
+        ],
+      ),
+    );
+    return AnimatedOpacity(
+      opacity: state.value ? 1 : 0,
+      duration: const Duration(milliseconds: 200),
+      child: Offstage(
+        offstage: !state.value,
+        child: Acrylic(
+          blurAmount: 1,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: text,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   void debugFillProperties(final DiagnosticPropertiesBuilder properties) {
