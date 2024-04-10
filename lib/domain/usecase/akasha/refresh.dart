f344d00f10f8852cae3f0ca47615bec2a347a44f
Traveling Lumine <107802667+traveling-lumine@users.noreply.github.com>
2024-04-10 17:39:26 +0900

    Delete unused files

--- lib/domain/usecase/akasha/refresh.dart
+++
@@ -1,9 +0,0 @@
-import 'package:genshin_mod_manager/domain/entity/akasha.dart';
-import 'package:genshin_mod_manager/domain/repo/akasha.dart';
-
-final class AkashaRefreshUseCase {
-  AkashaRefreshUseCase({required final NahidaliveAPI api}) : _api = api;
-  final NahidaliveAPI _api;
-
-  Future<List<NahidaliveElement>> call() => _api.fetchNahidaliveElements();
-}
