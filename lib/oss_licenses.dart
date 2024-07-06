5e9632a4507738ca126e897d3ba8d88c463f7cb5
traveling-lumine <107802667+traveling-lumine@users.noreply.github.com>
2024-07-06 21:10:46 +0900

    Bump

--- lib/oss_licenses.dart
+++ lib/oss_licenses.dart
@@ -34,6 +34,7 @@ const allDependencies = <Package>[
   _code_builder,
   _collection,
   _convert,
+  _cp949_codec,
   _cross_file,
   _crypto,
   _custom_lint,
@@ -159,6 +160,7 @@ const dependencies = <Package>[
   _archive,
   _cached_network_image,
   _collection,
+  _cp949_codec,
   _desktop_drop,
   _ffi,
   _filepicker_windows,
@@ -1585,6 +1587,47 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
     dependencies: [PackageRef('typed_data')]
   );
 
+/// cp949_codec 1.0.2
+const _cp949_codec = Package(
+    name: 'cp949_codec',
+    description: 'The best package for cp949 decoding/encoding. support null-safety, less memory comsumption. include examples and tests',
+    repository: 'https://github.com/letyletylety/cp949_codec',
+    authors: [],
+    version: '1.0.2',
+    license: '''BSD 3-Clause License
+
+Copyright (c) 2021, Lety
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
+    isMarkdown: false,
+    isSdk: false,
+    dependencies: []
+  );
+
 /// cross_file 0.3.4+1
 const _cross_file = Package(
     name: 'cross_file',
@@ -3004,13 +3047,13 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
     dependencies: [PackageRef('flutter'), PackageRef('intl'), PackageRef('scroll_pos'), PackageRef('recase'), PackageRef('math_expressions')]
   );
 
-/// flutter 3.22.1
+/// flutter 3.22.2
 const _flutter = Package(
     name: 'flutter',
     description: 'A framework for writing Flutter applications',
     homepage: 'https://flutter.dev',
     authors: [],
-    version: '3.22.1',
+    version: '3.22.2',
     license: '''Copyright 2014 The Flutter Authors. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
