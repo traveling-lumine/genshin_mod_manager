1c9836ee705b08e2385ff73f4de12517182a22de
traveling-lumine <107802667+traveling-lumine@users.noreply.github.com>
2024-09-21 02:48:28 +0900

    Add mod image size control

--- lib/src/ui/widget/time_aware_image.dart
+++ lib/src/ui/widget/time_aware_image.dart
@@ -4,6 +4,8 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:image_size_getter/file_input.dart';
+import 'package:image_size_getter/image_size_getter.dart';
 
 import '../../di/fs_watcher.dart';
 
@@ -15,12 +17,14 @@ class TimeAwareFileImage extends HookConsumerWidget {
     this.fit = BoxFit.contain,
   });
   final String path;
-  final BoxFit? fit;
+  final BoxFit fit;
   final ImageFrameBuilder? frameBuilder;
 
   @override
   Widget build(final BuildContext context, final WidgetRef ref) {
     final curMTime = useState<int>(DateTime.now().microsecondsSinceEpoch);
+    final imageSize =
+        useState<Size>(ImageSizeGetter.getSize(FileInput(File(path))));
 
     final eventStream =
         ref.watch(fileEventWatcherProvider(path, detectModifications: true));
@@ -31,6 +35,7 @@ class TimeAwareFileImage extends HookConsumerWidget {
           (final event) async {
             await FileImage(File(path)).evict();
             curMTime.value = DateTime.now().microsecondsSinceEpoch;
+            imageSize.value = ImageSizeGetter.getSize(FileInput(File(path)));
           },
         );
 
@@ -39,11 +44,35 @@ class TimeAwareFileImage extends HookConsumerWidget {
       [eventStream],
     );
 
-    return Image.file(
-      File(path),
-      key: ValueKey(curMTime.value),
-      fit: fit,
-      frameBuilder: frameBuilder,
+    return LayoutBuilder(
+      builder: (final context, final constraints) {
+        final candidateWidth = constraints.maxWidth.ceil();
+        final candidateHeight = constraints.maxHeight.ceil();
+        final candidateAspectRatio = candidateWidth / candidateHeight;
+
+        final imageSize_ = imageSize.value;
+        final imageWidth = imageSize_.width;
+        final imageHeight = imageSize_.height;
+        final imageAspectRatio = imageWidth / imageHeight;
+
+        final cacheWidth = imageAspectRatio < candidateAspectRatio &&
+                candidateWidth < imageWidth
+            ? null
+            : candidateWidth;
+        final cacheHeight = imageAspectRatio < candidateAspectRatio &&
+                candidateHeight < imageHeight
+            ? candidateHeight
+            : null;
+
+        return Image.file(
+          File(path),
+          key: ValueKey(curMTime.value),
+          fit: fit,
+          frameBuilder: frameBuilder,
+          cacheWidth: cacheWidth,
+          cacheHeight: cacheHeight,
+        );
+      },
     );
   }
 
