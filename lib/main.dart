3390e93ecf5d2066dbd33ba0aba4ef28ee170d4b
traveling-lumine <107802667+traveling-lumine@users.noreply.github.com>
2024-09-23 04:25:09 +0900

    Pull hero route out of shell route

--- lib/main.dart
+++ lib/main.dart
@@ -65,6 +65,16 @@ class _MyAppState extends ConsumerState<_MyApp> {
         path: RouteNames.firstpage.name,
         builder: (final context, final state) => const FirstRoute(),
       ),
+      GoRoute(
+        name: RouteNames.categoryHero.name,
+        path:
+            '${RouteNames.categoryHero.name}/:${RouteParams.categoryHeroTag.name}',
+        pageBuilder: (final context, final state) {
+          final heroTag =
+              state.pathParameters[RouteParams.categoryHeroTag.name]!;
+          return heroPage(context, heroTag);
+        },
+      ),
       ShellRoute(
         builder: (final context, final state, final child) =>
             HomeShell(child: child),
@@ -89,47 +99,6 @@ class _MyAppState extends ConsumerState<_MyApp> {
               return CategoryRoute(
                 categoryName: categoryName,
                 key: ValueKey(categoryName),
-              );
-            },
-          ),
-          GoRoute(
-            name: RouteNames.categoryHero.name,
-            path:
-                '${RouteNames.categoryHero.name}/:${RouteParams.categoryHeroTag.name}',
-            pageBuilder: (final context, final state) {
-              final heroTag =
-                  state.pathParameters[RouteParams.categoryHeroTag.name]!;
-              return CustomTransitionPage(
-                barrierDismissible: true,
-                opaque: false,
-                fullscreenDialog: true,
-                transitionDuration: const Duration(milliseconds: 200),
-                transitionsBuilder: (
-                  final context,
-                  final animation,
-                  final secondaryAnimation,
-                  final child,
-                ) =>
-                    AnimatedBuilder(
-                  animation: animation,
-                  builder: (final context, final child) {
-                    const mult = 6;
-                    return BackdropFilter(
-                      filter: ImageFilter.blur(
-                        sigmaX: animation.value * mult,
-                        sigmaY: animation.value * mult,
-                      ),
-                      child: child,
-                    );
-                  },
-                  child: child,
-                ),
-                child: GestureDetector(
-                  onTap: context.pop,
-                  onSecondaryTap: context.pop,
-                  child:
-                      Hero(tag: heroTag, child: ModPreviewImage(path: heroTag)),
-                ),
               );
             },
           ),
@@ -170,3 +139,39 @@ class _MyAppState extends ConsumerState<_MyApp> {
     super.dispose();
   }
 }
+
+Page<dynamic> heroPage(
+  final BuildContext context,
+  final String heroTag,
+) =>
+    CustomTransitionPage(
+      barrierDismissible: true,
+      opaque: false,
+      fullscreenDialog: true,
+      transitionDuration: const Duration(milliseconds: 200),
+      transitionsBuilder: (
+        final context,
+        final animation,
+        final secondaryAnimation,
+        final child,
+      ) =>
+          AnimatedBuilder(
+        animation: animation,
+        builder: (final context, final child) {
+          const mult = 6;
+          return BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: animation.value * mult,
+              sigmaY: animation.value * mult,
+            ),
+            child: child,
+          );
+        },
+        child: child,
+      ),
+      child: GestureDetector(
+        onTap: context.pop,
+        onSecondaryTap: context.pop,
+        child: Hero(tag: heroTag, child: ModPreviewImage(path: heroTag)),
+      ),
+    );
